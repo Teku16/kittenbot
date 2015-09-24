2f424f66536bc771ecf433a227804b640128d792
Mikey <teku1998@gmail.com>
2015-09-24 01:41:51 -0400

    rehaul handleTriviaV2.py again -much better comments/docs -updated FINISHED/TODO -source a_stopTrivia into stopTrivia -condensed some functions such as startTrivia

--- modules/handleTriviaV2.py
+++ modules/handleTriviaV2.py
@@ -9,21 +9,24 @@
         [-] maybe make hints reveal x per word rather than per string
         [-] make hints reveal more if len(answer) > x
         [-] stop genHints from trying to reveal a ' ' -- possibly if answer[i] == ' ': remove it from [unused] so it cannot be "revealed"
-        [-] transform a_stopTrivia into stopTrivia
+        [-] ability to grab all possible messages from the bot DB
+        [-] possibly move the timesUp timer from askQuestion to genHints...
+        [-] add !restoreTrivia to reinstall last DB.. before adding to it again -- for debugging
     FINISHED:
         [✔] build base model
         [✔] listen for !trivia and !stoptrivia commands, make them function properly
         [✔] read database for questions
-        [✔] interpret total number of questions for "Question #[num/maxnum]" format :) - that was really fun
-        [✔] fix listening for answers -- thanks TheRandomDog and Levtastic/Lev!
+        [✔] interpret total number of questions for "#[num/maxnum]" format
+        [✔] fix listening for answers -- thanks TheRandomDog and Lev!
         [✔] make working & safe question loop that doenst crash the bot entirely -- thanks Lev!
         [✔] stop automatically after x unanswered questions
         [✔] add channel !commands to: [✔] reset, [✔] add, and [✔] remove trivia question database
         [✔] changed the need to write trivia_question = num/question/answer -- it gets the length of the DB list now
         [✔] make handleHints notice spaces -- thanks Lev -- again! :D
+        [✔] source a_stopTrivia into stopTrivia
 """
 import random
-def init(): #required by each module, initializes the module to the bot
+def init():             #required by each module, initializes the module to the bot
     handleTrivia()
     
 class handleTrivia():
@@ -48,7 +51,7 @@ class handleTrivia():
         elif event.arguments[0].startswith("!stop"):
             #print("got to stop") #debug
             self.trivia_stopper = event.source.nick
-            self.stopTrivia(bot, connection, event)
+            self.stopTrivia(bot, connection, event, auto = False)
     #CORRECT
         elif self.questionLoopRunning:
             #print("got to correct") #debug
@@ -87,9 +90,7 @@ class handleTrivia():
                             bot.send(connection, event.target, "Sorry %s, the question '%s' does not exist in the database." % (event.source.nick, q_to_remove), event)
                         else:
                             bot.db.delete('trivia_question', q_to_remove)
-                            bot.send(connection, event.target, "Okay, %s, I'll remove that from the database!" % event.source.nick, event)
-                        
-                            
+                            bot.send(connection, event.target, "Okay, %s, I'll remove that from the database!" % event.source.nick, event)                            
     #CONFIRM
         elif self.resetInProgress == True and self.trivia_resetter in event.source.nick:
             #print("got to confirm") #debug
@@ -112,90 +113,76 @@ class handleTrivia():
     def startTrivia(self, bot, connection, event):
         if self.questionLoopRunning:
             return # trivia is already started
-            
-        num_of_questions = len(bot.db.get_all('trivia_question'))
-        if num_of_questions < 1:
+
+        if len(bot.db.get_all('trivia_question')) < 1:
             bot.send(connection, event.target, "Oh no! There aren't any questions in the database.", event)
             return
-            
-        started_message = ("Trivia has been started by " + self.trivia_starter + "! Here we go!")
-        bot.send(connection, event.target, started_message, event)
-        
         self.questionLoopRunning = True
-        bot.execute_delayed(connection, 3, self.askQuestion, (bot, connection, event))
+        started_message = ("Trivia has been started by " + self.trivia_starter + "! Here we go!") #set message
+        bot.send(connection, event.target, started_message, event)                                #send message
+        bot.execute_delayed(connection, 3, self.askQuestion, (bot, connection, event))            #in 3 seconds ask a question
         
-    def stopTrivia(self, bot, connection, event):
+    def stopTrivia(self, bot, connection, event, auto = False):
         if not self.questionLoopRunning:
             bot.send(connection, event.target, "Trivia is already stopped! Use !trivia to start it!", event)
             return
         self.questionLoopRunning = False
         self.num_answered = 0
         self.question_num = 0
-        
-        stopped_message = ("Trivia has been stopped by " + self.trivia_stopper + ". Type !trivia to start again!")
-        bot.send(connection, event.target, stopped_message, event)
-        
-    def a_stopTrivia(self, bot, connection, event):
-        if not self.questionLoopRunning:
-            return
-        self.questionLoopRunning = False
-        self.num_answered = 0
-        self.question_num = 0
-        bot.send(connection, event.target, "Oops! Nobody guessed it. That's three questions unanswered! Stopping trivia.", event)
+        if not auto: #used by normal trivia stop
+            bot.send(connection, event.target, "Trivia has been stopped by %s. Type !trivia to start again!" % self.trivia_stopper, event)
+        else:        #used by 3 question timeout    
+            bot.send(connection, event.target, "Oops! Nobody guessed it. That's three questions unanswered, so I'm stopping trivia!", event)
         
     def askQuestion(self, bot, connection, event):
         if not self.questionLoopRunning:
-            return # we have been told to stop asking questions
+            return                                      #if we arent in a trivia loop, exit this function
             
-        #get a list [] of all question/answer strings in the DB
-        all_strings = bot.db.get_all('trivia_question')
-        #get the total number of questions in the DB
-        total_questions_num = len(all_strings)
+        all_strings = bot.db.get_all('trivia_question') #get a list [] of all question/answer strings in the DB
+        total_questions_num = len(all_strings)          #get the total number of questions in the DB
         
-        #pick a random question/answer string from the all_strings list []; then store what number in the list it is
-        QA_string = random.choice(all_strings)
-        q_number = all_strings.index(QA_string)
+        QA_string = random.choice(all_strings)          #pick a random question/answer string from the all_strings list [];
+        q_number = all_strings.index(QA_string)         #then store what number in the list it is
         
-        #split and evaluate QA_string into question and self.answer
-        QA_string = QA_string.split('/')
+        QA_string = QA_string.split('/')                #split and evaluate QA_string into question and self.answer
         question = QA_string[0]
-        self.answer = QA_string[1] #needs to be self to be accessed in other functions
+        self.answer = QA_string[1]                      #needs to be self to be accessed in other functions
 
-        q_message = ("Question [#%s/%d]: %s" % (q_number + 1, total_questions_num, question))
-        bot.send(connection, event.target, q_message, event)
-
-        bot.execute_delayed(connection, 20, self.timesUp, (self.question_num, bot, connection, event))
-        self.genHints(self.answer, self.question_num, bot, connection, event)
+        q_message = ("Question [#%s/%d]: %s" % (q_number + 1, total_questions_num, question))          #set the message string
+        bot.send(connection, event.target, q_message, event)                                           #send the message string
+        
+        bot.execute_delayed(connection, 20, self.timesUp, (self.question_num, bot, connection, event)) #set a timesUp limit
+        self.genHints(self.answer, self.question_num, bot, connection, event)                          #start the hint loop
         
     def correctAnswer(self, bot, connection, event):
-        self.num_unanswered = 0
-        self.possible_letters = []
+        self.num_unanswered = 0         #answering it right breaks the "auto stop" count
+        self.possible_letters = []      #sanity check for genHints --maybe unnecessary
         bot.send(connection, event.target, "Hooray! %s was correct! The answer was '%s'." % (event.source.nick, str(self.answer)), event)
-        self.question_num += 1
-        connection.execute_delayed(2, self.askQuestion, (bot, connection, event))
-        self.answer = ''
+        self.question_num += 1          #sanity check for several functions, makes sure that the module advances and cannot get stuck
+        connection.execute_delayed(2, self.askQuestion, (bot, connection, event)) #wait 2 seconds then ask another question
+        self.answer = ''                #not sure why this is here.. --maybe unncessary
         
     def timesUp(self, question_num, bot, connection, event):
-        if self.question_num == question_num and self.questionLoopRunning:
-            self.num_unanswered += 1
-            self.possible_letters = []
-            self.question_num += 1
-            if not self.num_unanswered > 2:
+        if self.question_num == question_num and self.questionLoopRunning: #if same question, and still going...
+            self.num_unanswered += 1         #advance number, genHints sanity check
+            self.possible_letters = []       #reset list used in genHints
+            self.question_num += 1           #increase question number for checks
+            if not self.num_unanswered > 2:  #if 3 questions are unanswered...
                 bot.send(connection, event.target, "Oops! Nobody guessed it. The answer was '%s'... Here comes the next question!" % str(self.answer), event)
                 connection.execute_delayed(3, self.askQuestion, (bot, connection, event))
-            else:
-                self.a_stopTrivia(bot, connection, event)
+            else:                            #if it hasnt been 3 unanswered, and we run out of time
+                self.stopTrivia(bot, connection, event, auto = True)
             
     def genHints(self, answer, question_num, bot, connection, event):
         if self.question_num != question_num or not self.questionLoopRunning:
-            return
-        unused = []
-        self.possible_letters = []
+            return 
+        unused = []                 #clear this so that it doesnt create a huuuge list
+        self.possible_letters = []  #^
         try:
-            for x in range(1,4):
-                unused = [i for i in range(len(answer)) if i not in self.possible_letters] #       -----\
-                self.possible_letters.extend(random.sample(unused, x)) #                                 DEFINE VARs/ATTRIBUTES
-                time = x * 10 / 2                                                          #       -----/
+            for x in range(1,4):                                                           #cycle through 1,2,3
+                unused = [i for i in range(len(answer)) if i not in self.possible_letters] #cant explain this at 1:30am...
+                self.possible_letters.extend(random.sample(unused, x))                     #add some numbers into possible letters to reveal
+                time = x * 10 / 2                                                          #x = either 1,2,3, so we get 5,10,15
                 hint = ''.join([(i in self.possible_letters or answer[i] == ' ') and answer[i] or '-' for i in range(len(answer))])       #GENERATE HINT
                 bot.execute_delayed(connection, time, self.sendHints, (question_num, connection, event.target, "[Hint #%s/3]: %s" % (x, hint), event))  #SEND HINT
         except BaseException as e:
@@ -204,6 +191,6 @@ class handleTrivia():
             
     def sendHints(self, question_num, connection, target, message, event):
         if question_num != self.question_num or not self.questionLoopRunning:
-            return
-        #print(str(self.question_num) + ", " + str(question_num)) #debug
-        bot.send(connection, target, message, event)
+            return #if we've moved on via stop/start/answer or the trivia loop is off
+
+        bot.send(connection, target, message, event) #send hint message
