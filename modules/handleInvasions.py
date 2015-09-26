a268ef502d7328866d1ce668e0bd577f36346a25
Mikey <teku1998@gmail.com>
2015-09-26 02:21:38 -0400

    apply updates to nameGenerator.py and handleInvasions.py
    -nameGenerator: added triggers, moved list generation to one time on init
    -handleInvasions: module can now list districts with invasions, module is more organized, has triggers

--- modules/handleInvasions.py
+++ modules/handleInvasions.py
@@ -3,30 +3,45 @@
 #      https://github.com/Levtastic/kittenbot      #
 """
     TODO:
-        [-] init functions
-        [-] call invasions api
-        [-] have module interpret api data 
+        [-] display cogtypes, remaining, etc
+        [-] return a message when there arent any invasions
+        [-] have module interpret api data _properly_
     FINISHED:
-        [✔] 
+        [✔] init functions
+        [✔] call invasions api
 """
-import urllib.request
+import urllib.request as r,json
 def init():
-    #invasionHandler()
-    pass
+    invasionHandler()
+
 class invasionHandler():
 
     def __init__(self):
         event_handler.hook('irc:on_pubmsg', self.on_pubmsg)
-        pass
+        self.triggers = ["puppy are there any invasions?", "puppy are there any invasions"]
         
     def on_pubmsg(self, bot, connection, event):
         if event.target not in ("#squadchat", "#puppy"):
             return
     #LIST INV
-        if event.arguments[0].startswith("puppy are there any invasions?"):
+        if any(trigger in event.arguments[0] for trigger in self.triggers):
             self.returnInvasions(bot, connection, event)
             
     def returnInvasions(self, bot, connection, event):
-        raw_data = urllib.request.urlopen("https://www.toontownrewritten.com/api/invasions").read().decode().split("b'")
-        invasions = raw_data.decode().split('invasions', 1)
-        types = invasions[1].split('"type":')
+        districts = []
+        invargs = []
+        stats = []
+        raw_json = json.loads(r.urlopen("https://www.toontownrewritten.com/api/invasions").read().decode('utf-8'))
+
+        dist_str = ', '.join([a for a in raw_json['invasions']])
+        
+        # for a in invasions:
+            # districts += a
+            # for b in invasions[a]:
+                # invargs += b
+                # try:
+                    # for c in invasions[a][b]:
+                        # stats += c
+                # except:
+                    # pass
+        bot.send(connection, event.target, "Hey there %s, there are currently invasions in %s!" % (event.source.nick, dist_str), event)
--- modules/nameGenerator.py
+++ modules/nameGenerator.py
@@ -3,7 +3,6 @@
 #      https://github.com/Levtastic/kittenbot      #
 """
     TODO:
-        [-] have module select titles, middle, last names etc
         [-] add possibility for specific name genders "puppy what should I name my girl/boy toon"
         [-] give generation better randomness somehow
         [-] OPTIMIZE
@@ -11,6 +10,7 @@
         [✔] init functions
         [✔] build a name file
         [✔] set the lists with a FOR loop
+        [✔] have module select titles, middle, last names etc
 """
 import random
 from random import randint
@@ -21,7 +21,8 @@ class generateName():
 
     def __init__(self):
         event_handler.hook('irc:on_pubmsg', self.on_pubmsg)
-        self.triggers = ['puppy what should I name my toon?', 'what should I name my toon?']
+        self.triggers = ['puppy what should I name my toon?', 'what should I name my toon?', 'puppy what should I name my toon', '!namegen']
+        self.generateLists()
     
     def on_pubmsg(self, bot, connection, event):
         if event.target not in ("#squadchat", "#puppy"):
@@ -31,50 +32,52 @@ class generateName():
             
     def generateName(self, bot, connection, event):
         randomizer = randint(0,1000)
-        maleTitle = []
-        femaleTitle = []
-        neutralTitle = []
-        maleFirst = []
-        femaleFirst = []
-        neutralFirst = []
-        lastPrefix = []
-        lastName = []
-        lastSuffix = []
+        if randomizer < 250:
+            #print("<25")
+            generatedName = random.choice(self.firstNames) + " " + random.choice(self.lastName)
+        elif randomizer < 500:
+            #print("<50")
+            generatedName = random.choice(self.titles) + " " + random.choice(self.firstNames) + " " + random.choice(self.lastName)
+        elif randomizer < 750:
+            #print("<75")
+            generatedName = random.choice(self.titles) + " " + random.choice(self.firstNames) + " " + random.choice(self.lastName) + random.choice(self.lastSuffix)
+        elif randomizer < 1000:
+            #print("<100")
+            generatedName = random.choice(self.titles) + " " + random.choice(self.firstNames) + " " + random.choice(self.lastPrefix) + random.choice(self.lastName) + random.choice(self.lastSuffix)
+        
+        bot.send(connection, event.target, "%s, the name I came up with is '%s'!" % (event.source.nick, generatedName))
+        
+    def generateLists(self):
+        self.maleTitle = []
+        self.femaleTitle = []
+        self.neutralTitle = []
+        self.maleFirst = []
+        self.femaleFirst = []
+        self.neutralFirst = []
+        self.lastPrefix = []
+        self.lastName = []
+        self.lastSuffix = []
         with open('NamesDB.txt') as ndb:
             nameList = [q.rstrip('\n') for q in ndb]
             for a in nameList:
                 num = a.split("*")[1]
                 if num == '0':
-                    maleTitle += [a.split("*")[2]]
+                    self.maleTitle += [a.split("*")[2]]
                 elif num == '1':
-                    femaleTitle += [a.split("*")[2]]
+                    self.femaleTitle += [a.split("*")[2]]
                 elif num == '2':
-                    neutralTitle += [a.split("*")[2]]
+                    self.neutralTitle += [a.split("*")[2]]
                 elif num == '3':
-                    maleFirst += [a.split("*")[2]]
+                    self.maleFirst += [a.split("*")[2]]
                 elif num == '4':
-                    femaleFirst += [a.split("*")[2]]
+                    self.femaleFirst += [a.split("*")[2]]
                 elif num == '5':
-                    neutralFirst += [a.split("*")[2]]
+                    self.neutralFirst += [a.split("*")[2]]
                 elif num == '6':
-                    lastPrefix += [a.split("*")[2]]
+                    self.lastPrefix += [a.split("*")[2]]
                 elif num == '7':
-                    lastName += [a.split("*")[2]]
+                    self.lastName += [a.split("*")[2]]
                 elif num == '8':
-                    lastSuffix += [a.split("*")[2]]
-        titles = maleTitle + femaleTitle + neutralTitle
-        firstNames = maleFirst + femaleFirst + neutralFirst
-        if randomizer < 250:
-            print("<25")
-            generatedName = random.choice(firstNames) + " " + random.choice(lastName)
-        elif randomizer < 500:
-            print("<50")
-            generatedName = random.choice(titles) + " " + random.choice(firstNames) + " " + random.choice(lastName)
-        elif randomizer < 750:
-            print("<75")
-            generatedName = random.choice(titles) + " " + random.choice(firstNames) + " " + random.choice(lastName) + random.choice(lastSuffix)
-        elif randomizer < 1000:
-            print("<100")
-            generatedName = random.choice(titles) + " " + random.choice(firstNames) + " " + random.choice(lastPrefix) + random.choice(lastName) + random.choice(lastSuffix)
-        
-        bot.send(connection, event.target, "%s, the name I came up with is '%s'!" % (event.source.nick, generatedName))
+                    self.lastSuffix += [a.split("*")[2]]
+        self.titles = self.maleTitle + self.femaleTitle + self.neutralTitle
+        self.firstNames = self.maleFirst + self.femaleFirst + self.neutralFirst
